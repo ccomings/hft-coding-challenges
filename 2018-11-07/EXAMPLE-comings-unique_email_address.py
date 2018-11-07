e79449f8fbaab88ac8d698ef74ba81c8bb1684fb
Chris Comings <chris.comings@lagunitas.com>
2018-11-07 11:32:02 -0800

    better solution

--- 2018-11-07/EXAMPLE-comings-unique_email_address.py
+++ 2018-11-07/EXAMPLE-comings-unique_email_address.py
@@ -1,15 +1,8 @@
 def numUniqueEmails(self, emails):
-    a = {}
+    seen = set()
     for email in emails:
-        new_email = email
-        email_to_add = ''
-        for letter in email:
-            if letter == ".":
-                new_email = new_email[1:]
-            elif letter == "@":
-                break
-            else:
-                email_to_add += (new_email[0])
-                new_email = new_email[1:]
-        a[new_email] = email_to_add
-    return len(a)
+        local, _, domain = email.partition('@')
+        if '+' in local:
+            local = local[:local.index('+')]
+        seen.add(local.replace('.','') + '@' + domain)
+    return len(seen)
