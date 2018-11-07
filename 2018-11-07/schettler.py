c9d30c7c15f868e23e058b290de8b36c1c321d4b
SF <mschettler@users.noreply.github.com>
2018-11-07 11:59:54 -0800

    Update schettler.py

--- 2018-11-07/schettler.py
+++ 2018-11-07/schettler.py
@@ -1,8 +1,7 @@
 def main():
 
     def divide(dividend, divisor):
-        ret = 0
-        while 1:
+        for ret in range(dividend):
             dividend -= abs(divisor)
             if dividend < 0:
                 if divisor < 0:
@@ -10,8 +9,9 @@ def main():
                 return ret
             ret += 1
 
-    print divide(10, 3)
-    print divide(7, -3)
+    assert divide(10, 3) == 3
+    assert divide(7, -3) == -2
+
 
 if __name__ == '__main__':
     main()
