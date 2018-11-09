21320992bdde210166ebf269514a10790f4d0fbd
Matthew Wyatt <matthew.wyatt@lagunitas.com>
2018-11-08 21:42:18 -0800

    Refactor for easier reading

--- 2018-11-07/wyatt/n-sum.rkt
+++ 2018-11-07/wyatt/n-sum.rkt
@@ -2,7 +2,9 @@
 
 (define test-list '(-1 0 1 2 -1 -4))
 
+(define (lists-same? a b) (equal? (list->set a) (list->set b)))
+
 (define (n-sum nums count goal)
-  (remove-duplicates (filter-map (lambda (xs) (and (= goal (apply + xs)) xs)) (combinations nums count)) (lambda (a b) (equal? (list->set a) (list->set b)))))
+  (remove-duplicates (filter-map (lambda (xs) (and (= goal (apply + xs)) xs)) (combinations nums count)) lists-same?))
 
 
