4270cf0e00a200286f4515bdfcd06d1a39009c56
Matthew Wyatt <matthew.wyatt@lagunitas.com>
2018-11-09 17:39:09 -0800

    Add some driver calls

--- 2018-11-07/wyatt/n-sum.rkt
+++ 2018-11-07/wyatt/n-sum.rkt
@@ -1,7 +1,5 @@
 #lang racket
 
-(define test-list '(-1 0 1 2 -1 -4))
-
 (define (lists-same? a b) (equal? (list->set a) (list->set b)))
 
 (define (n-sum nums count goal)
@@ -12,3 +10,10 @@
    lists-same?))
 
 
+(n-sum '(-1 0 1 2 -1 -4) 3 0)
+
+(n-sum '(-2 3 0 1 1 -1) 3 0)
+
+(n-sum '(1 2 3 4) 3 0)
+
+(n-sum '(1 2 3) 3 6)
