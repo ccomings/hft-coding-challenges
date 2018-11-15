4a63c858e92b0c56faaa0175f4eb8f5fd45d86db
RFortney88 <RossFortney@gmail.com>
2018-11-15 10:26:50 -0800

    Update fortney_combinations.py

--- 2018-11-07/fortney_combinations.py
+++ 2018-11-07/fortney_combinations.py
@@ -1,26 +1,11 @@
 
 # coding: utf-8
 
-# In[27]:
-
-
 import itertools
-
-
-# In[29]:
-
 
 nums = (-1, 0, 1, 2, -1, -4)
 
-
-# In[33]:
-
-
 combos_list = list(itertools.combinations(nums, 3))
-
-
-# In[34]:
-
 
 def zero_checker(combos):
     
@@ -32,9 +17,6 @@ def zero_checker(combos):
             sum_zero_combos.append(comb)
     
     return sum_zero_combos
-
-
-# In[35]:
 
 
 zero_checker(combos_list)
