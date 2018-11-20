ecf0ed92ad905735d248fc516e7988ec219f27f5
Sean K. Roth <therothinator@gmail.com>
2018-11-19 19:07:26 -0800

    clear set instead of redefine

--- 2018-11-15/skroth/substr.kt
+++ 2018-11-15/skroth/substr.kt
@@ -1,13 +1,13 @@
 fun longestSub(str: String): Int {
     var maxLen = 0
     var currLen = 0
-    var currSet: MutableSet<Char> = mutableSetOf()
+    val currSet: MutableSet<Char> = mutableSetOf()
 
     for (char in str) {
         currSet.add(char)
         currLen += 1
         if (currSet.size < currLen) {
-            currSet = mutableSetOf()
+            currSet.clear()
             currLen = 0
         }
         maxLen = Math.max(maxLen, currLen)
