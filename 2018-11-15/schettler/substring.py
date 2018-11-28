691f3c61ef0ebe4c0d6d2a16f20427f59b6d7490
Matt Schettler <matt.schettler@lagunitas.com>
2018-11-28 11:24:42 -0800

    fix

--- 2018-11-15/schettler/substring.py
+++ 2018-11-15/schettler/substring.py
@@ -2,15 +2,15 @@
 #
 # Matt Schettler
 # Nov 2018
-# 
+#
 # https://github.com/ccomings/hft-coding-challenges/blob/master/2018-11-15/longest-substr-wo-repeating-chars.md
-# 
+#
 import itertools
 
 
 def len_longest_substr_no_repeat_chars(s):
     """ return the length of the longest substring of s that does not repeat any characters """
-    return max((len(s[i:j]) for i, j in itertools.permutations(range(len(s)+1), 2) if i < j and (j - i) == len(set(s[i:j]))))
+    return max((len(s[i:j]) for i, j in itertools.permutations(range(len(s)+1), 2) if (j - i) == len(set(s[i:j]))))
 
 
 def main():
@@ -22,7 +22,7 @@ def main():
     assert len_longest_substr_no_repeat_chars('abcabcbb') == 3  # abc
     assert len_longest_substr_no_repeat_chars('bbbbb') == 1  # b
     assert len_longest_substr_no_repeat_chars('pwwkew') == 3  # kew
-    
+
     # advanced tests
     assert len_longest_substr_no_repeat_chars('hello world, this is earth') == 11  # world, this
     assert len_longest_substr_no_repeat_chars(''.join(map(str, range(50)))) == 10  # 2345678910
